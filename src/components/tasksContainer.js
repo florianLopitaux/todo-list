3d48f5011fb50dd0b1954855e0d13610f039f102
Florian <florian.lopitaux@etu.univ-amu.fr>
2023-03-13 11:59:58 +0100

    Build style front-end with tasks examples

--- src/components/tasksContainer.js
+++ src/components/tasksContainer.js
@@ -1,10 +1,37 @@
 import { Component } from "react";
+import Task from "./task.js";
 
 class TasksContainer extends Component {
     render() {
         return (
             <main>
-                
+                <Task
+                    title={"1. Idée"}
+                />
+                <Task
+                    title={"2. Marché"}
+                />
+                <Task
+                    title={"3. Wireframe"}
+                />
+                <Task
+                    title={"4. Design"}
+                />
+                <Task
+                    title={"5. Landingpage"}
+                />
+                <Task
+                    title={"6. Développement"}
+                />
+                <Task
+                    title={"7. Publish"}
+                />
+                <Task
+                    title={"8. Pub"}
+                />
+                <Task
+                    title={"9. Feedback"}
+                />
             </main>
         );
     }
